8730259f9d19c630ef3290911d2108560ba439a1
davitazzio <davide.tazzioli@studio.unibo.it>
2022-05-30 10:06:50 +0200

    added author notes

--- genetic-super-mario-bross/neural_network/my_nn.py
+++ genetic-super-mario-bross/neural_network/my_nn.py
@@ -1,9 +1,18 @@
+"""
+    @Tazzioli Davide - davide.tazzioli@studio.unibo.it
+"""
 import torch
 import torch.nn as nn
 
 
 class SuperMarioNeuralNetwork(nn.Sequential):
     def __init__(self, input_size, hidden1_size, num_classes):
+        """
+
+        :param input_size: int
+        :param hidden1_size: int
+        :param num_classes: int
+        """
         self.input_size = input_size
         super(SuperMarioNeuralNetwork, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden1_size, bias=True)
@@ -11,12 +20,21 @@ class SuperMarioNeuralNetwork(nn.Sequential):
         self.fc2 = nn.Linear(hidden1_size, num_classes, bias=True)
 
     def forward(self, x):
+        """
+
+        :param x: game status
+        :return: Torch: output of the network
+        """
         out = self.fc1(x)
         out = self.relu1(out)
         out = self.fc2(out)
         return out
 
     def get_input_size(self):
+        """
+        Get the input size of the neural network
+        :return: int: input size
+        """
         return self.input_size
 
     def set_params(self, chromosome):
